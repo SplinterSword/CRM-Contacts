98dc1841f078dfb5cbd59c99da1c8192c2aa97f0
SplinterSword <sparshj2003@gmail.com>
2024-11-14 19:45:22 +0530

    Created Frontend

---
+++ frontend/app/contact-form/page.tsx
@@ -0,0 +1,14 @@
+import ContactForm from '@/components/ContactForm'
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Navbar />
+      <ContactForm />
+    </div>
+  )
+}
+
+export default page
--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -7,9 +7,14 @@ const Navbar = () => {
       <nav className="bg-blue-600 text-white p-4">
         <div className="container mx-auto flex justify-between items-center">
           <span>Welcome, User</span>
-          <Link href="/contact-form" className="hover:underline">
-            Contact Form
-          </Link>
+          <div className='flex gap-10'>
+            <Link href="/home" className="hover:underline">
+              DashBoard
+            </Link>
+            <Link href="/contact-form" className="hover:underline">
+              Contact Form
+            </Link>
+          </div>
         </div>
       </nav>
     </div>
